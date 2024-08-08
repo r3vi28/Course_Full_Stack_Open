b88d24caeb6f6ad9ec718c5e7aecbf66797114e5
r3vi28 <moisesjr3vi@gmail.com>
2024-08-08 16:02:30 -0400

    I completed the exercise 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -34,22 +34,48 @@ const Header = ({course}) => {
 const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
   return (
     <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part1 part1={part1} exercises1={exercises1}/>
+      <Part2 part2={part2} exercises2={exercises2}/>
+      <Part3 part3={part3} exercises3={exercises3}/>
     </div>
   )
 }
 
 const Total = ({exercises1, exercises2, exercises3}) => {
   return (
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    <div>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  )
+}
+
+const Part1 = ({part1, exercises1}) => {
+  return (
+    <div>
+      <p>
+        {part1} {exercises1}
+      </p>
+    </div>
+  )
+}
+
+const Part2 = ({part2, exercises2}) => {
+  return (
+    <div>
+      <p>
+        {part2} {exercises2}
+      </p>
+    </div>
+  )
+}
+
+const Part3 = ({part3, exercises3}) => {
+  return (
+    <div>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </div>
   )
 }
 
