30ba8849d475e7fa1316d1d1a71f1f5a623fb7d9
r3vi28 <moisesjr3vi@gmail.com>
2024-08-09 19:21:25 -0400

    I completed exercise 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,47 +1,54 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course}/>
       <Content
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
+        part1={part1}
+        part2={part2}
+        part3={part3}
       />
       <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
+        exercises1={part1.exercises}
+        exercises2={part2.exercises}
+        exercises3={part3.exercises}
         />
     </div>
   )
 }
 
 const Header = ({course}) => {
-
+  console.log(course)
   return (
     <h1>{course}</h1>
   )
 }
 
-const Content = ({part1, exercises1, part2, exercises2, part3, exercises3}) => {
+const Content = ({part1, part2, part3}) => {
   return (
     <div>
-      <Part1 part1={part1} exercises1={exercises1}/>
-      <Part2 part2={part2} exercises2={exercises2}/>
-      <Part3 part3={part3} exercises3={exercises3}/>
+      <Part part={part1} />
+      <Part part={part2} />
+      <Part part={part3} />
     </div>
   )
 }
 
 const Total = ({exercises1, exercises2, exercises3}) => {
+  console.log(exercises1, "+" ,exercises2, "+", exercises3)
   return (
     <div>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
@@ -49,31 +56,12 @@ const Total = ({exercises1, exercises2, exercises3}) => {
   )
 }
 
-const Part1 = ({part1, exercises1}) => {
+const Part = ({part}) => {
+  console.log(part)
   return (
     <div>
       <p>
-        {part1} {exercises1}
-      </p>
-    </div>
-  )
-}
-
-const Part2 = ({part2, exercises2}) => {
-  return (
-    <div>
-      <p>
-        {part2} {exercises2}
-      </p>
-    </div>
-  )
-}
-
-const Part3 = ({part3, exercises3}) => {
-  return (
-    <div>
-      <p>
-        {part3} {exercises3}
+        {part.name} {part.exercises}
       </p>
     </div>
   )
